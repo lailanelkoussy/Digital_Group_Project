4ad63fbf59853344a8b31ca83701b325d37819fc
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2018-03-02 17:15:48 +0200

    Finished input extraction algorithm

--- main.cpp
+++ main.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    string input;
-    int preloc= 0, size, currloc;
+    string input,temp="";
+    vector <string> storage ;
+    int preloc= 0;
+    unsigned long size;
 
     cout<<"insert:";
-        cin>>input;
+    getline(cin,input);
+    cout<<input<<endl;
     size = input.size();
     if (size>15)
         cout<<"Invalid input";
 
+
+
     for (int i = 0; i<size; i++)
-    { if (i==size-1){
+    {
+        if ((input[i] ==',')|| (i == size-1))
+        {
+            for (int j = preloc; j < i; j++)
+                temp += input[j];
+            if(i==size-1)
+                temp+=input[i];
+            cout<<temp<<endl;
+            storage.push_back(temp);
+            temp = "";
+            preloc = i+1;
 
-
-        }
-        if (input[i] ==',')
-        { for (int j = 0; j< i - preloc; i++)
-            {
-
-            }
         }
     }
-
-
+    cout<<"The minterms entered were: ";
+    for (auto element:storage)
+        cout<<element;
 
 
 
